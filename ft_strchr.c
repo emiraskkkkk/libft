416b272bd8eaca6fbd6514a420a0e0aa9802bc6d
emiraskkkkk <emiraskkk@outlook.com>
2026-08-16 23:08:38 +0300

    ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -17,11 +17,11 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == (char) c)
+		if (s[i] == (unsigned char)c)
 			return ((char *) &s[i]);
 		i++;
 	}
 	if ((char) c == '\0')
-		return ((char *) &s[i]);
+		return ((char *)&s[i]);
 	return (0);
 }
